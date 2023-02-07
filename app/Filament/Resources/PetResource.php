c3bc5b726b64d737f959bb61e1bbbc2c292ffd66
mickaelmarques14 <mickaelmarques14@gmail.com>
2023-02-07 22:34:17 +0000

    simple pet resource

---
+++ app/Filament/Resources/PetResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PetResource\Pages;
+use App\Filament\Resources\PetResource\RelationManagers;
+use App\Models\Pet;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PetResource extends Resource
+{
+    protected static ?string $model = Pet::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('entry_date')
+                    ->required(),
+                Forms\Components\TextInput::make('weight'),
+                Forms\Components\TextInput::make('height'),
+                Forms\Components\DatePicker::make('birth_date'),
+                Forms\Components\TextInput::make('color')
+                    ->maxLength(50),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('entry_date')
+                    ->date(),
+                Tables\Columns\TextColumn::make('weight'),
+                Tables\Columns\TextColumn::make('height'),
+                Tables\Columns\TextColumn::make('birth_date')
+                    ->date(),
+                Tables\Columns\TextColumn::make('color'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\ForceDeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPets::route('/'),
+            'create' => Pages\CreatePet::route('/create'),
+            'edit' => Pages\EditPet::route('/{record}/edit'),
+        ];
+    }    
+    
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
