17a12f7f5fd76723cd405a151b78750cbf276f9e
mickaelmarques14 <mickaelmarques14@gmail.com>
2025-02-05 23:23:57 +0000

    infolist clinic

--- app/Filament/Resources/ClinicResource/Pages/ViewClinic.php
+++ app/Filament/Resources/ClinicResource/Pages/ViewClinic.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\ClinicResource\Pages;
 use App\Filament\Resources\ClinicResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Infolists\Components;
+use Filament\Infolists\Infolist;
 
 class ViewClinic extends ViewRecord
 {
@@ -17,4 +19,24 @@ class ViewClinic extends ViewRecord
         ];
     }
 
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Components\Section::make()
+                    ->schema([
+                        Components\Split::make([
+                            Components\Grid::make(2)
+                                ->schema([
+                                    Components\Group::make([
+                                        Components\TextEntry::make('name'),
+                                        Components\IconEntry::make('status')
+                                            ->translateLabel()
+                                            ->boolean(),
+                                    ]),
+                                ]),
+                        ])->from('lg'),
+                    ]),
+            ]);
+    }
 }
